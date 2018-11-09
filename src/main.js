378d704ec36615cd02b84ab03e5c09b855926075
Anks0905 <1183821556@qq.com>
2018-11-09 18:00:47 +0800

    1\完成脚手架首页布局

--- src/main.js
+++ src/main.js
@@ -1,8 +1,40 @@
 import Vue from 'vue'
 import App from './App.vue'
 
-Vue.config.productionTip = false
+// 引入全局css样式路径
+import './assets/statics/site/css/style.css';
+
+
+// 引入路由相关
+import VueRouter from 'vue-router';
+
+// 引入index主页
+import index from './components/index.vue';
+
+// use一下 
+Vue.use(VueRouter);
+
+// 写规则
+const routes = [
+  {
+  path: '/',
+  component: index
+},
+  {
+  path: '/index',
+  component: index
+}
+]
+
+// 实例化路由对象
+const router = new VueRouter({
+  // 规则挂载到实例上
+  routes
+})
 
 new Vue({
   render: h => h(App),
+  // 路由挂载到vue实例上
+  router
+
 }).$mount('#app')
